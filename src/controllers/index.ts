d32a897185fc6b187732eef80540c8c5e3fca8ea
kiwicarlos <carlos@kiwicredito.com>
2023-12-16 02:52:42 -0500

    Update .prettierrc.json and add getSingleCharacter route

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,16 +2,17 @@ import { Request, Response } from 'express'
 
 export const getCharacters = async (req: Request, res: Response) => {
   try {
-    const { species } = req.body
-    console.log(species, 'species')
-
+    const { species, page } = req.body
     const response = await fetch('https://rickandmortyapi.com/graphql', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         query: `
                 query {
-                    characters(filter: { species: "${species}" }) {
+                    characters(page: ${page}, filter: { species: "${species}" }) {
+                      info {
+                        count
+                      }
                       results {
                         id
                         name
@@ -32,9 +33,39 @@ export const getCharacters = async (req: Request, res: Response) => {
       }),
     })
     const data = await response.json()
-    console.log(data, 'data data')
-
-    res.status(200).send(data.data.characters.results)
+    res.status(200).send({
+      info: data.data.characters.info,
+      characters: data.data.characters.results,
+    })
+  } catch (error) {
+    res.status(500).send('Error in server:' + error)
+  }
+}
+export const getSingleCharacter = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const response = await fetch('https://rickandmortyapi.com/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: `
+                query {
+                    charactersByIds(ids: ${id}) {
+                      id
+                      name
+                      status
+                      species
+                      image
+                      origin {
+                        name
+                        }
+                      }
+                  }
+                `,
+      }),
+    })
+    const data = await response.json()
+    res.status(200).send(data.data.charactersByIds[0])
   } catch (error) {
     res.status(500).send('Error in server:' + error)
   }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 const router = Router()
-import { getCharacters } from '../controllers/index'
+import { getCharacters, getSingleCharacter } from '../controllers/index'
 
 router.post('/characters', getCharacters)
+router.get('/get-single-character/:id', getSingleCharacter)
 
 export default router
